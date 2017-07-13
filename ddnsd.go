f2dbb78060fa015fcf73520ecd8fc16cf75856e7
Thomas Kerpe <toke@toke.de>
2017-07-13 21:07:48 +0200

    Crude running version

--- ddnsd.go
+++ ddnsd.go
@@ -25,6 +25,18 @@ type Config struct {
 	Secret     string `yaml:"Secret"`
 }
 
+/*
+Example Config
+
+---
+Listen: ":8080"
+Nameserver: a.ns.example.com:53
+TTL: 3600
+Zone: home.example.com.
+Token: test
+Secret: BASE64SECRET
+*/
+
 type Error interface {
 	error
 	Status() int
@@ -41,7 +53,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	path := flag.String("config", "/etc/ddns.conf", "path to config file, eg. /home/user/ddns.conf")
+	path := flag.String("config", "/etc/ddnsd.conf", "path to config file, eg. /home/user/ddnsd.conf")
 	flag.Parse()
 
 	conf, err := ioutil.ReadFile(*path)
@@ -63,10 +75,6 @@ func main() {
 	http.ListenAndServe(c.Listen, nil)
 }
 
-type Message struct {
-	Text string
-}
-
 func makednsupdate(cfg Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
@@ -76,7 +84,7 @@ func makednsupdate(cfg Config) http.HandlerFunc {
 		hostname := r.URL.Query().Get("hostname")
 		token := r.URL.Query().Get("token")
 
-		var validHostname = regexp.MustCompile(`^[a-z0-9.\-]{2,10}$`)
+		var validHostname = regexp.MustCompile(`^[a-z0-9.\-]{2,100}$`)
 
 		if token != cfg.Token {
 			http.Error(w, "Not Authorized", http.StatusForbidden)
@@ -87,17 +95,20 @@ func makednsupdate(cfg Config) http.HandlerFunc {
 			http.Error(w, "Mandatory fields not set", http.StatusBadRequest)
 			return
 		}
+
 		if !validHostname.MatchString(hostname) {
 			http.Error(w, "Invalid Hostname", http.StatusBadRequest)
 			return
 		}
 
-		// Only allow suffixed
+		// Only allow suffixed hostnames
 		if strings.HasSuffix(hostname, cfg.Zone) {
 			fqdnhost = hostname
 		} else {
 			fqdnhost = hostname + "." + cfg.Zone
 		}
+
+		fmt.Printf("Request for %s -> %s\n", fqdnhost, ip)
 
 		rr := new(dns.A)
 		rr.Hdr = dns.RR_Header{Name: fqdnhost, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: uint32(cfg.TTL)}
@@ -116,14 +127,16 @@ func makednsupdate(cfg Config) http.HandlerFunc {
 
 		reply, _, err := c.Exchange(m, cfg.Nameserver)
 		if err != nil {
+			fmt.Printf("Error %s\n", err)
 			http.Error(w, err.Error(), http.StatusFailedDependency)
 		}
 		if reply != nil && reply.Rcode != dns.RcodeSuccess {
+			fmt.Printf("Error %s\n", dns.RcodeToString[reply.Rcode])
 			http.Error(w, "Something went wrong", 400)
 			fmt.Fprint(w, fmt.Errorf("DNS update failed. Server replied: %s", dns.RcodeToString[reply.Rcode]))
 			return
 		}
-
+		fmt.Println("OK")
 		fmt.Fprint(w, "OK")
 	}
 }
